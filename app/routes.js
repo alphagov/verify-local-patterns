0dbc9de6113e1a3636deb5828f1441420c39f202
Sanjay Poyzer <sanjaypoyzer@gmail.com>
2016-12-23 13:48:57 +0000

    routing for photo question

--- app/routes.js
+++ app/routes.js
@@ -27,4 +27,17 @@ router.get('/pay/choose-payment', function (req, res) {
 
 });
 
+router.get('/service-patterns/concessionary-travel/example-service/photo/photo-guide', function (req, res) {
+  // get the answer from the query string (eg. ?over18=false)
+  var answer = req.query.answer
+
+  if (answer === 'skip') {
+    res.redirect('upload')
+  } else if (answer === 'shop') {
+    res.redirect('photo-shop')
+  } else {
+    res.render('service-patterns/concessionary-travel/example-service/photo/photo-guide')
+  }
+})
+
 module.exports = router
