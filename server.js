a77361919efbe46fe4d76680a4b9f5b6d32035b2
Sanjay Poyzer <sanjaypoyzer@gmail.com>
2017-02-21 17:25:35 +0000

    improvements to upload photo page

--- server.js
+++ server.js
@@ -135,8 +135,6 @@ if (useAutoStoreData === 'true') {
       }
     })
 
-    console.log('sessiondata is',req.session.data);
-
     // set council variable to all of relevant councilData
     if(req.session.data.councilChoice){
       for(var i in councilsData){
